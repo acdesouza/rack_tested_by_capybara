8e67f9eee4ec6b13e7ef2cff6a639f60417711ad
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2015-09-12 11:15:41 -0300

    Bundle require, basead on RACK_ENV.

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,42 @@
 require_relative 'boot'
-require 'rack-server-pages'
 
-class RackAppWithCapybara
-  def self.application
-    @app = Rack::Builder.new do
-      use Rack::ServerPages do |config|
-        config.view_path = 'app/views'
+module RackAppWithCapybara
+  class << self
+    @app = nil
+
+    def application
+      @app ||= Rack::Builder.new do
+        use Rack::ServerPages do |config|
+          config.view_path = 'app/views'
+        end
+
+        run Rack::ServerPages::NotFound
+      end.to_app
+
+      @app
+    end
+
+    def env
+      rack_env = ENV['RACK_ENV'] ? ENV['RACK_ENV'].strip : "development"
+
+      def rack_env.method_missing(name, *args, &block)
+        false
       end
 
-      run Rack::ServerPages::NotFound
-    end.to_app
+      if !rack_env.empty?
+        rack_env.define_singleton_method("#{rack_env}?") { true }
+      end
 
-    @app
+      rack_env
+    end
+
+    def groups
+      env = RackAppWithCapybara.env
+      groups = [:default, env]
+    end
   end
 end
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*RackAppWithCapybara.groups)
